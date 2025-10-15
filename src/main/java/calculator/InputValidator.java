3f796b950f592213e704957204ed194ebdab3e1f
geoKim <kgeo6264@gmail.com>
2025-10-15 12:17:11 +0900

    feat: throw exception for invalid input

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -2,12 +2,23 @@ package calculator;
 
 public class InputValidator {
 
+    //입력 값이 빈 칸이 아닌지 확인하는 메서드
     public static boolean isBlank(String input) {
-        if (input.length() == 0){
-            return true;
+        return input.isEmpty();
+    }
+
+    //기본 연산자가 존재하는지 확인하는 메서드
+    public static boolean existDefault(String input) {
+        for (char c : input.toCharArray()) {
+            if (c == ':' || c == ',') {
+                return true;
+            }
         }
-        else {
-            return false;
-        }
+        return false;
+    }
+
+    //커스텀 연산자가 존재하는지 확인하는 메서드
+    public static boolean existCustom(String input){
+        return input.startsWith("//") && input.contains("\n");
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,9 +5,16 @@ public class StringCalculator {
     public static int calculate(String str) {
 
         /**
-         *  String이 존재하지 않으면 IllegalArgumentException을 반환합니다.
+         *  예외 처리
          */
+
+        // 빈칸이거나 null이면 IllegalArgumentException을 발생
         if (InputValidator.isBlank(str)) {
+            throw new IllegalArgumentException();
+        }
+
+        // 기본연산자와 커스텀연산자를 가지고 있지 않으면 IllegalArgumentException을 발생
+        if (!(InputValidator.existDefault(str) || InputValidator.existCustom(str))){
             throw new IllegalArgumentException();
         }
 
