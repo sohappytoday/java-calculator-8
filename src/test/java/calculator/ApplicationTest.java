f6ab7301cb2400e13dc923d45c3a143844b3a7e1
geoKim <kgeo6264@gmail.com>
2025-10-19 01:24:09 +0900

    test: Implement test case of special case

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,62 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 여러_커스텀_연산자_호출() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2a:3,4//a\\n");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 커스텀_연산자_어디서든_호출() {
+        assertSimpleTest(() -> {
+            run("1;2//;\\n:3:4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 커스텀_연산자_여러문자_허용() {
+        assertSimpleTest(() -> {
+            run("//ab\\n1ab2ab3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 커스텀_연산자_null_또는_숫자_불가능() {
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//\\n1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//1\\n1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 커스텀_연산자에_슬래시_허용_바_엔_불가() {
+        assertSimpleTest(() -> {
+            run("////\\n1//2//3//4");
+            assertThat(output()).contains("결과 : 10");
+        });
+        assertSimpleTest(() -> {
+            assertThatThrownBy(() -> runException("//\\n\\n1,2,3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @Test
+    void 커스텀_연산자에_기본연산자_포함_가능() {
+        assertSimpleTest(() -> {
+            run("//:,\\n1:,2:,3,4:5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
